4e4f7037e7558ef3adfe3c6d5360ebb5f496cafa
Victoria Cheng <victoria.cheng15@gmail.com>
2025-02-11 16:40:01 -0700

    refactor login page (#3)
    * remove unused style and edit title container style
    
    * rename to login from home and remove unused imports
    
    * add header shown false option

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,7 @@
 import { Tabs } from "expo-router";
-import React from "react";
 import { Platform } from "react-native";
 
-import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol } from "@/components/ui/IconSymbol";
-import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 
 export default function TabLayout() {
@@ -13,8 +10,6 @@ export default function TabLayout() {
 			screenOptions={{
 				tabBarActiveTintColor: Colors.light.tint,
 				headerShown: false,
-				tabBarButton: HapticTab,
-				tabBarBackground: TabBarBackground,
 				tabBarStyle: Platform.select({
 					ios: {
 						// Use a transparent background on iOS to show the blur effect
@@ -27,7 +22,7 @@ export default function TabLayout() {
 			<Tabs.Screen
 				name="index"
 				options={{
-					title: "Home",
+					title: "Login",
 					tabBarIcon: ({ color }) => (
 						<IconSymbol size={28} name="house.fill" color={color} />
 					),
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,11 @@ import "react-native-reanimated";
 export default function RootLayout() {
 	return (
 		<ThemeProvider value={DefaultTheme}>
-			<Stack>
+			<Stack
+				screenOptions={{
+					headerShown: false,
+				}}
+			>
 				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 				<Stack.Screen name="+not-found" />
 			</Stack>
